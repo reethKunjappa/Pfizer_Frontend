13c8a4ec4b7c150fa18dbba1decd94b4d6abd7c6
reethKunjappa <rchintha@ITS.JNJ.com>
2019-11-05 18:51:08 +0530

    Sandbox Node Server Url changed - Shashank

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://3.90.245.202:3005'
+  serverUrl: 'http://palr.datafoundry.ai:3005',  
+  // serverUrl: 'http://3.90.245.202:3005'
 };
